9afa16937102a2f7e29e3ad98daff926f0848600
CandelarioRamosj <145868728+CandelarioRamosj@users.noreply.github.com>
2023-10-11 18:40:47 -0600

    se agrego la funcion closed esta funcion cierra la barra de navegacion en dispositivos tablet o movil

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function clean(){
   document.getElementById('formulario').reset()
 }
+
+function closed(){
+  const menuToggle = document.getElementById('navbarSupportedContent')
+  const bsCollapse = new bootstrap.Collapse(menuToggle)
+  bsCollapse.toggle()
+}
